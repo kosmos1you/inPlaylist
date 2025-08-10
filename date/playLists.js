ed60d96b60586d6525522a551985340fa3999d04
kosmos404 <kosmos1you@gmail.com>
2025-08-11 00:08:43 +0300

    Adding css and html markup to blocks

--- date/playLists.js
+++ date/playLists.js
@@ -9,28 +9,28 @@ export let playLists = [
                 artist: 'Eminem',
                 title: ' Rap God',
                 isHot: true,
-                imageSource: '/img/cardImage/trackList/track1.jpeg',
+                imageSource: 'img/cardImage/trackList/track1.jpeg',
                 link: '/audio/Eminem - Rap God.mp3'
             },
             {
                 artist: 'Eminem 2',
                 title: ' Rap God 2',
                 isHot: true,
-                imageSource: '/img/cardImage/trackList/track1.jpeg',
+                imageSource: 'img/cardImage/trackList/track1.jpeg',
                 link: '/audio/Eminem - Rap God.mp3'
             },
             {
                 artist: '50 cent ',
                 title: ' In da Club',
                 isHot: false,
-                imageSource: '/img/cardImage/trackList/track2.jpeg',
+                imageSource: 'img/cardImage/trackList/track2.jpeg',
                 link: '/audio/50cent - In da club.mp3'
             },
             {
                 artist: '50 cent 2 ',
                 title: ' In da Club 2',
                 isHot: false,
-                imageSource: '/img/cardImage/trackList/track2.jpeg',
+                imageSource: 'img/cardImage/trackList/track2.jpeg',
                 link: '/audio/50cent - In da club.mp3'
             },
         ]
@@ -44,14 +44,14 @@ export let playLists = [
                 artist: 'Public Enemy',
                 title: ' Fight the Power',
                 isHot: true,
-                imageSource: '/img/cardImage/trackList/track3.jpeg',
+                imageSource: 'img/cardImage/trackList/track3.jpeg',
                 link: '/audio/Public Enemy - Fight The Power.mp3'
             },
             {
                 artist: 'Public Enemy 2',
                 title: ' Fight the Power 2',
                 isHot: true,
-                imageSource: '/img/cardImage/trackList/track3.jpeg',
+                imageSource: 'img/cardImage/trackList/track3.jpeg',
                 link: '/audio/Public Enemy - Fight The Power.mp3'
             },
         ]
